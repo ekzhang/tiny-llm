f39b80cc0af8594b7ca7de10a3cdbf537be296a8
Eric Zhang <ekzhang1@gmail.com>
2025-09-07 15:18:42 -0400

    Days 1 and 2 implementations

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -1,37 +1,66 @@
 import mlx.core as mx
-from .basics import softmax, linear
+from .basics import linear, softmax
 
 
 def scaled_dot_product_attention_simple(
-    query: mx.array,
-    key: mx.array,
-    value: mx.array,
+    query: mx.array,  # [N.., L_query, D]
+    key: mx.array,  # [N.., L_key, D]
+    value: mx.array,  # [N.., L_key, D]
     scale: float | None = None,
     mask: mx.array | None = None,
-) -> mx.array:
-    pass
+) -> mx.array:  # [N.., L_query, D]
+    if scale is None:
+        scale = mx.rsqrt(query.shape[-1])
+
+    # [N.., L_query, L_key]
+    attn_logits = mx.matmul(query, key.swapaxes(-2, -1)) * scale
+    if mask is not None:
+        attn_logits += mask
+
+    attn_weights = softmax(attn_logits, axis=-1)  # [N.., L_query, L_key]
+    return mx.matmul(attn_weights, value)  # [N.., L_query, D]
 
 
 class SimpleMultiHeadAttention:
     def __init__(
         self,
-        hidden_size: int,
-        num_heads: int,
-        wq: mx.array,
-        wk: mx.array,
-        wv: mx.array,
-        wo: mx.array,
+        hidden_size: int,  # E
+        num_heads: int,  # H
+        wq: mx.array,  # [H x D, E]
+        wk: mx.array,  # [H x D, E]
+        wv: mx.array,  # [H x D, E]
+        wo: mx.array,  # [E, H x D]
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
 
     def __call__(
         self,
-        query: mx.array,
+        query: mx.array,  # [N.., L, E]
         key: mx.array,
         value: mx.array,
         mask: mx.array | None = None,
     ) -> mx.array:
-        pass
+        *batch_dims, l_dim, e_dim = query.shape
+
+        # [N.., L, H, D]
+        nlhd_shape = (*batch_dims, l_dim, self.num_heads, -1)
+
+        q = linear(query, self.wq).reshape(nlhd_shape)
+        k = linear(key, self.wk).reshape(nlhd_shape)
+        v = linear(value, self.wv).reshape(nlhd_shape)
+
+        # [N.., H, L, D]
+        q = q.swapaxes(-3, -2)
+        k = k.swapaxes(-3, -2)
+        v = v.swapaxes(-3, -2)
+        attn = scaled_dot_product_attention_simple(q, k, v, None, mask)
+
+        return linear(attn.swapaxes(-3, -2).reshape((*batch_dims, l_dim, -1)), self.wo)
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -4,14 +4,49 @@ import mlx.core as mx
 class RoPE:
     def __init__(
         self,
-        dims: int,
-        seq_len: int,
+        dims: int,  # D
+        seq_len: int,  # L
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
+        # [D // 2]
+        theta = mx.power(base, -mx.arange(0, dims, 2, dtype=mx.float32) / dims)
+        # [L, D // 2]
+        freqs = mx.arange(seq_len)[:, mx.newaxis] * theta
+        self.sin_freqs = mx.sin(freqs)
+        self.cos_freqs = mx.cos(freqs)
+        self.traditional = traditional
 
     def __call__(
-        self, x: mx.array, offset: list[slice] | slice | None = None
+        self,
+        x: mx.array,  # [N, L, H, D]
+        offset: list[slice] | slice | None = None,
     ) -> mx.array:
-        pass
+        N, L, H, D = x.shape
+
+        seq_idx: slice | mx.array  # [L] or [N, L]
+        if offset is None:
+            seq_idx = slice(0, L)
+        elif isinstance(offset, slice):
+            assert offset.stop - offset.start == L, f"offset must be of length {L}"
+            seq_idx = offset
+        elif isinstance(offset, list):
+            for o in offset:
+                assert o.stop - o.start == L, f"offset must be of length {L}"
+            seq_idx = mx.array([list(range(i.start, i.stop)) for i in offset])
+
+        cos_basis = self.cos_freqs[seq_idx].reshape(-1, L, 1, D // 2)
+        sin_basis = self.sin_freqs[seq_idx].reshape(-1, L, 1, D // 2)
+
+        if self.traditional:
+            x = x.reshape(N, L, H, D // 2, 2)
+            x0, x1 = x[..., 0], x[..., 1]
+        else:
+            x = x.reshape(N, L, H, 2, D // 2)
+            x0, x1 = x[..., 0, :], x[..., 1, :]
+
+        y0 = x0 * cos_basis - x1 * sin_basis
+        y1 = x0 * sin_basis + x1 * cos_basis
+        return mx.stack([y0, y1], axis=-1 if self.traditional else -2).reshape(
+            N, L, H, D
+        )
